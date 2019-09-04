04878899929758d395fccbfe771c343d85a4f90c
wlight <wormside@gmail.com>
2019-09-04 13:15:43 +0000

    修改和删除收货地址

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerPolicies();
+
+        // 使用 Gate::guessPolicyNamesUsing 方法来自定义策略文件寻找逻辑
+        Gate::guessPolicyNamesUsing(function ($class){
+            // class_basename 是laravel 提供的一个辅助函数，可以获取类的简短名称
+            // 例如传入 \App\Models\User 会返回 User
+            return '\\App\\Polices\\'.class_basename($class).'Plicy';
+        });
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,5 +19,6 @@
     </div>
     {{--JS脚本--}}
     <script src="{{ mix('js/app.js') }}"></script>
+    @yield('scriptsAfterJs')
 </body>
 </html>
