ff8738ad11c459898744df1035f1a4ed54a5bfd1
wlight <wormside@gmail.com>
2019-09-06 08:20:13 +0000

    初始化管理后台

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,13 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
-        // 使用 Gate::guessPolicyNamesUsing 方法来自定义策略文件寻找逻辑
-        Gate::guessPolicyNamesUsing(function ($class){
-            // class_basename 是laravel 提供的一个辅助函数，可以获取类的简短名称
-            // 例如传入 \App\Models\User 会返回 User
-            return '\\App\\Polices\\'.class_basename($class).'Plicy';
-        });
+        //
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // 使用 Gate::guessPolicyNamesUsing 方法来自定义策略文件寻找逻辑
+        Gate::guessPolicyNamesUsing(function ($class){
+            // class_basename 是laravel 提供的一个辅助函数，可以获取类的简短名称
+            // 例如传入 \App\Models\User 会返回 User
+            return '\\App\\Polices\\'.class_basename($class).'Plicy';
+        });
     }
 }
