ad33218d0b053a57765e2643678878c945793af0
wlight <wormside@gmail.com>
2019-12-01 01:29:56 +0000

    用户界面 - 订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 use App\Http\Requests\OrderRequest;
 use App\Exceptions\InvalidRequestException;
 use App\Jobs\CloseOrder;
+use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
@@ -59,5 +60,16 @@ class OrdersController extends Controller
 
         $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
         return $order;
+    }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
     }
 }
